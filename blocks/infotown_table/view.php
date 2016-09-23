da31fed3222309e576b36fdf9d15020dd0de7a81
Hiroshi Sawai <info@info-town.jp>
2016-09-24 07:16:32 +0900

    Remove css class 'table' in table markup

--- blocks/infotown_table/view.php
+++ blocks/infotown_table/view.php
@@ -1,10 +1,10 @@
 <?php defined('C5_EXECUTE') or die( "Access Denied." ); ?>
 <?php if (count($rows) > 0 && $rowsLength > 0 && $colsLength > 0) : ?>
-    <table class="table">
+    <table>
         <?php for ($i = 0; $i < $rowsLength; $i++) : ?>
             <tr>
                 <?php for ($j = 0; $j < $colsLength; $j++) : ?>
-                    <?php if ((string)$rows[$i * $colsLength + $j]['th'] === 'true') : ?>
+                    <?php if ((string) $rows[$i * $colsLength + $j]['th'] === 'true') : ?>
                         <th><?php echo h($rows[$i * $colsLength + $j]['content']); ?></th>
                     <?php else : ?>
                         <td><?php echo h($rows[$i * $colsLength + $j]['content']); ?></td>
