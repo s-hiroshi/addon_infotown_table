4d945f01c39bd361edf3dab5838e4ba5da6efcf7
Hiroshi Sawai <info@info-town.jp>
2016-09-30 06:40:54 +0900

    Change log

--- controller.php
+++ controller.php
@@ -16,7 +16,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'infotown_table';
     protected $appVersionRequired = '5.7.4';
-    protected $pkgVersion = '0.9.5';
+    protected $pkgVersion = '0.9.6';
     protected $pkgAutoloaderMapCoreExtensions = true;
 
     public function getPackageDescription()
